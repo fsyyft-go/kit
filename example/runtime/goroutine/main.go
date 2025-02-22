1610101c7a0df661f81ccd68210d6a53e105cb64
FanFusheng <ffs@ppno.net>
2025-02-22 15:20:53 +0800

    feat(runtime/goroutine): 增加使用示例

--- example/runtime/goroutine/main.go
+++ example/runtime/goroutine/main.go
@@ -6,37 +6,26 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"strings"
 	"sync"
-)
 
-// GetGoID 获取当前 goroutine 的 ID。
-func GetGoID() uint64 {
-	var buf [64]byte
-	// 获取当前 goroutine 的调用栈。
-	n := runtime.Stack(buf[:], false)
-	// 调用栈的第一行包含 goroutine ID。
-	idField := strings.Fields(strings.TrimPrefix(string(buf[:n]), "goroutine "))[0]
-	var id uint64
-	fmt.Sscanf(idField, "%d", &id)
-	return id
-}
+	"github.com/fsyyft-go/kit/runtime/goroutine"
+)
 
 func main() {
 	// 创建一个等待组，用于等待所有 goroutine 完成。
 	var wg sync.WaitGroup
 
 	// 打印主 goroutine 的 ID。
-	fmt.Printf("主 goroutine ID: %d\n", GetGoID())
+	fmt.Printf("主 goroutine ID: %d\n", goroutine.GetGoID()) // nolint:staticcheck
 
 	// 启动 3 个新的 goroutine。
 	for i := 0; i < 3; i++ {
 		wg.Add(1)
 		go func(index int) {
 			defer wg.Done()
-			id := GetGoID()
-			fmt.Printf("goroutine %d 的 ID: %d\n", index+1, id)
+			id := goroutine.GetGoID() // nolint:staticcheck
+			idSlow := goroutine.GetGoIDSlow()
+			fmt.Printf("goroutine %d 的 ID: %d %d\n", index+1, id, idSlow)
 		}(i)
 	}
 
