fc856b209417752ea37a5157b3eee9dadf447d94
FanFusheng <ffs@ppno.net>
2025-02-22 16:15:05 +0800

    fix(runtime/goroutine): 修正 windows 系统下的编译异常

--- runtime/goroutine/goid_windows_arm64.go
+++ runtime/goroutine/goid_windows_arm64.go
@@ -11,9 +11,3 @@ func GetGoID() int64 {
 	// TODO 汇编的方法未实现，先使用开销较大的。
 	return getGoIDSlow()
 }
-
-// GetGoIDSlow 获取当前协程的 ID，当无法从 GetGoID 获取协程 ID 时使用此方法。
-// 该方法通过获取协程的堆栈信息，然后解析堆栈信息来提取协程 ID。
-func GetGoIDSlow() int64 {
-	return getGoIDSlow()
-}
