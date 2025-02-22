8f558eee01042c8951089c4ed55a415d7ac628fb
FanFusheng <ffs@ppno.net>
2025-02-22 15:20:47 +0800

    fix(runtime/goroutine): 修正 arm64 的 build 错误

--- runtime/goroutine/goid_windows_arm64.go
+++ runtime/goroutine/goid_windows_arm64.go
@@ -1,18 +1,8 @@
-// Copyright (c) 2025 fsyyft-go
+// Copyright 2025 fsyyft-go
 //
-// Licensed under the MIT License (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://github.com/fsyyft-go/kit/blob/main/LICENSE
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
-//go:build linux && arm64
+//go:build windows && arm64
 
 package goroutine
 
