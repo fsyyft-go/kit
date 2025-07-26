0a5c21f8485b659020c6ce8c1bddbe2a98883b30
FanFusheng <ffs@ppno.net>
2025-07-26 10:19:57 +0800

    feat(database): 添加 SQLite 数据库基础类库
    - 新建 sqlite 文件夹及 sqlite.go 文件
    - 添加 SQLite 数据库操作的基础类库代码
    - 注释说明：TODO 表示尚需完成的具体实现

---
+++ database/sql/sqlite/sqlite.go
@@ -0,0 +1,7 @@
+// Copyright 2025 fsyyft-go
+//
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+package sqlite
+
+// TODO: SQLite 基础类库的实现，不能使用 CGO。
